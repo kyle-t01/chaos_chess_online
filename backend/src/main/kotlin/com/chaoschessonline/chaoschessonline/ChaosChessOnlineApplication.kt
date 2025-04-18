6a03b41e3050c5582c796bb0863a02472e1e4153
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-18 10:16:14 +1000

    project initial setup

---
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/ChaosChessOnlineApplication.kt
@@ -0,0 +1,11 @@
+package com.chaoschessonline.chaoschessonline
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChaosChessOnlineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChaosChessOnlineApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/chaoschessonline/chaoschessonline/ChaosChessOnlineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chaoschessonline.chaoschessonline
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChaosChessOnlineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
