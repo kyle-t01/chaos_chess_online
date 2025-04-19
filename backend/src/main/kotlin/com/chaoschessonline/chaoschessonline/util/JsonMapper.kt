7782e369e13adf47a7283b6af618baedf8337013
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-19 12:45:01 +1000

    add kdocs to JsonMapper class

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/util/JsonMapper.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/util/JsonMapper.kt
@@ -7,11 +7,24 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.TextMessage
 
+/**
+ * Json mapper
+ *
+ * singleton class that manages conversion between Event <-> JSON <-> TextMessage
+ *
+ * @constructor Create empty Json mapper
+ */
 /* singleton class that manages conversion between Event <-> JSON <-> TextMessage */
 @Component
 class JsonMapper {
     private val mapper = jacksonObjectMapper()
 
+    /**
+     * Read TextMessage and return an Event
+     *
+     * @param message
+     * @return Event
+     */
     // helper function to parse Text Message to Event
     fun readTextMessage(message: TextMessage): Event {
         val json = mapper.readTree(message.payload)
@@ -22,6 +35,12 @@ class JsonMapper {
         return Event(type, data)
     }
 
+    /**
+     * Convert Event to TextMessage
+     *
+     * @param event
+     * @return TextMessage
+     */
     // convert Event to TextMessage
     fun convertToTextMessage(event: Event) : TextMessage {
         val json = mapper.writeValueAsString(event)
