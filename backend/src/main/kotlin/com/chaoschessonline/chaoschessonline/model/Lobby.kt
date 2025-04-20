e9aaa700083b86be3f38e6b9d7d5d8328669cc13
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-20 17:37:46 +1000

    add kdoc to Lobby class

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Lobby.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Lobby.kt
@@ -1,14 +1,27 @@
 package com.chaoschessonline.chaoschessonline.model
 
 import org.springframework.web.socket.WebSocketSession
-// should contain all active users
+
+/**
+ * Lobby
+ *
+ * Contains all actively connected players
+ *
+ * @property players
+ * @property isGameStarted
+ * @constructor Create empty Lobby
+ */
 class Lobby (
     private val players:MutableMap<WebSocketSession, Player> = mutableMapOf(),
     private var isGameStarted: Boolean = false
 )
 {
 
-
+    /**
+     * Remove player
+     *
+     * @param session
+     */
     fun removePlayer(session: WebSocketSession) {
         val player = players[session]
         println("$player LEFT the Lobby! [${players.size -1} players left...]")
@@ -21,10 +34,21 @@ class Lobby (
         return
     }
 
+    /**
+     * Get players
+     *
+     * @return
+     */
     fun getPlayers():List<Player> {
         return players.values.toList()
     }
 
+    /**
+     * Add to players
+     *
+     * @param session
+     * @param player
+     */
     fun addToPlayers(session: WebSocketSession, player: Player) {
         // associate the session with this player
         players[session] = player
@@ -32,12 +56,20 @@ class Lobby (
         return
     }
 
+    /**
+     * Start game
+     *
+     */
     fun startGame() {
         println("Game has officially started.")
         isGameStarted = true
         // start the game
     }
 
+    /**
+     * End game
+     *
+     */
     fun endGame() {
         println("Game has been terminated.")
         isGameStarted = false
