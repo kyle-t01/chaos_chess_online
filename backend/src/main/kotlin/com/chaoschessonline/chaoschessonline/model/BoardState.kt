5d29de8fb0b5c054a3712e84e5cbdd43fb4fb0ee
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-21 12:56:05 +1000

    BoardState has constructor for default boardstate

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/BoardState.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/BoardState.kt
@@ -5,22 +5,27 @@ import com.chaoschessonline.chaoschessonline.util.Vector2D
 /**
  * Board state
  *
+ * attackingDirection: South player attacks north = Vector2D.NORTH
+ *
  * @property parent
  * @property board
  * @property turnNumber
- * @property attackVector2D
+ * @property attackingDirection
  * @constructor Create Board state
  */
 data class BoardState(
     val parent: BoardState?,
     val board:Board,
     val turnNumber:Int,
-    val attackVector2D: Vector2D
+    val attackingDirection: Vector2D
 )
 {
     var children: List<BoardState> = emptyList()
     var eval:Int = 0
 
+    companion object {
+        fun defaultBoardState() = BoardState(null, Board.defaultBoard(), 0, Vector2D.NORTH)
+    }
 
 
 }
