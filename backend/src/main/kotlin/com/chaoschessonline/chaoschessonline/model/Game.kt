0c8183c51f5375ec5f6688a127346741206e2072
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-21 13:33:06 +1000

    add getters and setters for players of a Game

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Game.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Game.kt
@@ -1,9 +1,33 @@
 package com.chaoschessonline.chaoschessonline.model
 
+import com.chaoschessonline.chaoschessonline.util.Vector2D
+
 class Game()
 {
     private var currentState:BoardState = BoardState.defaultBoardState()
-    private val currentPlayers: MutableList<Player> = mutableListOf()
+    private var southPlayer: Player? = null
+    private var northPlayer: Player? = null
+
+    // getters and setters
+    fun getSouthPlayer(): Player? = southPlayer
+    fun getNorthPlayer(): Player? = northPlayer
+
+    fun setSouthPlayer(player: Player) {
+        this.southPlayer = player
+    }
+
+    fun setNorthPlayer(player: Player) {
+        this.northPlayer = player
+    }
+
+    fun setSouthPlayerAI(player: Player) {
+        this.southPlayer = Player("SOUTH AI", true, Vector2D.NORTH)
+    }
+
+    fun setNorthPlayerAI(player: Player) {
+        this.northPlayer = Player("NORTH AI", true, Vector2D.SOUTH)
+    }
+
 
     fun printGameState() = println(currentState)
 
