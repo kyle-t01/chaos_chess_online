6d6a85d4876cc404ac9045409baab268ee6f992e
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-22 14:42:01 +1000

    can convert piecetypes into a char

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/PieceType.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/PieceType.kt
@@ -1,17 +1,42 @@
 package com.chaoschessonline.chaoschessonline.model
 
+import com.chaoschessonline.chaoschessonline.util.Vector2D
+
+// define uppercase as UP (team north/ team up)
 enum class PieceType {
-    PAWN,
-    BISHOP,
-    KNIGHT,
-    ROOK,
-    QUEEN,
-    KING,
-    FOOT_SOLDIER,
-    SCHOLAR,
-    ADVISOR,
-    HORSE,
-    CHARIOT,
-    CANNON,
-    GENERAL,
+    PAWN, // P, p
+    BISHOP, // B, b
+    KNIGHT, // N, n
+    ROOK, // R, r
+    QUEEN, // Q, q
+    KING, // K, k
+    FOOT_SOLDIER, // Z, z (zu)
+    SCHOLAR, // S, s (shi)
+    ADVISOR, // X, x (xiang)
+    HORSE, // M, m (ma)
+    CHARIOT, // J, j (ju)
+    CANNON, // C, c (pao)
+    GENERAL,; // G, g
+
+    companion object{
+        fun toChar(type:PieceType, attackDirection:Vector2D):Char {
+            val isNorthPlayer:Boolean = (attackDirection == Vector2D.SOUTH)
+            val char = when(type) {
+                PAWN -> 'P'
+                BISHOP -> 'B'
+                KNIGHT -> 'N'
+                ROOK -> 'R'
+                QUEEN -> 'Q'
+                KING -> 'K'
+                FOOT_SOLDIER -> 'Z'
+                SCHOLAR -> 'S'
+                ADVISOR -> 'X'
+                HORSE -> 'M'
+                CHARIOT -> 'J'
+                CANNON -> 'C'
+                GENERAL -> 'G'
+            }
+            return if (isNorthPlayer) char else char.lowercaseChar()
+        }
+    }
 }
