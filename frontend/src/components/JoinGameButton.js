a1605daaf25379b027bd5c033cd1049de27f7d4c
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-23 16:11:46 +1000

    put join game button into separate functions

--- frontend/src/components/JoinGameButton.js
+++ frontend/src/components/JoinGameButton.js
@@ -6,15 +6,26 @@ const JoinGameButton = () => {
     // global state
     const { handlePlayerJoinNorth, handlePlayerJoinSouth, hasJoined, gameState } = GlobalVars();
 
+    const renderJoinSouthButton = () => {
+        return (
+            <button className="button" onClick={handlePlayerJoinSouth} hidden={gameState?.southPlayer ?? false} >
+                Join Game as Team South
+            </button>
+        );
+    }
 
+    const renderJoinNorthButton = () => {
+        return (
+            <button className="button" onClick={handlePlayerJoinNorth} hidden={gameState?.northPlayer ?? false}>
+                Join Game as Team North
+            </button>
+        );
+    }
     return (
-        <div className="join-game-buttons" hidden={hasJoined}>
-            <button className="button" onClick={handlePlayerJoinSouth} hidden={gameState?.southPlayer}>
-                Join Game (as Team South)
-            </button>
-            <button className="button" onClick={handlePlayerJoinNorth} hidden={gameState?.northPlayer}>
-                Join Game (as Team North)
-            </button>
+        <div className="join-game-buttons">
+            {renderJoinSouthButton()}
+            {renderJoinNorthButton()}
+
         </div>
     );
 
