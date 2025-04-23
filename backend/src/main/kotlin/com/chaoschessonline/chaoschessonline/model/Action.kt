1624e617c2e2d4c795718d37828faa6611fdf622
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-04-23 19:04:42 +1000

    add messages in require, when Action can't be formed from string

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Action.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/model/Action.kt
@@ -16,14 +16,20 @@ data class Action(
     val to:Vector2D,
 ) {
     companion object {
-        fun fromString(actionStr:String):Action? {
+        /**
+         * From string
+         *
+         * @param actionStr
+         * @return
+         */
+        fun fromString(actionStr:String):Action {
             // ie: "0,0 1,1"
             val posStrList:List<String> = actionStr.split(" ")
             val vList:MutableList<Vector2D> = mutableListOf()
             for (s in posStrList) {
                 vList.add(Vector2D.fromStr(s))
             }
-            require(vList.size == 2)
+            require(vList.size == 2) {"Error: Action could not be made from STRING"}
             // vList to Action
             val newFrom = vList[0]
             val newTo = vList[1]
