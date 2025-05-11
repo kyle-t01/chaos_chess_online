d33b7c964422fecb8b888572fa101c072140dc19
kylet-01 <90253433+kyle-t01@users.noreply.github.com>
2025-05-11 13:19:03 +1000

    util functions to help find where scholar pieces live

--- backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/util/Vector2D.kt
+++ backend/src/main/kotlin/com/chaoschessonline/chaoschessonline/util/Vector2D.kt
@@ -51,6 +51,41 @@ data class Vector2D(val col: Int, val row: Int)
         return (this - anchor).rotate() + anchor
     }
 
+    /**
+     * Find the size of a Vector2D dimension
+     *
+     * @return size
+     */
+    fun findSize(): Int {
+        return row * col
+    }
+
+    /**
+     * Within bounds
+     *
+     * @param dimension
+     * @return
+     */
+    fun withinBounds(dimension: Vector2D): Boolean {
+        val withinRow:Boolean = row >= 0 && row < dimension.row
+        val withinCol:Boolean = col >= 0 && col < dimension.col
+        return withinCol && withinRow
+    }
+
+    /**
+     * Find which half, either NORTH or SOUTH half a position belongs to
+     *
+     * @param dimension
+     * @return
+     */
+    fun findWhichHalf(dimension: Vector2D): Vector2D {
+        val halfRows = dimension.row / 2 // south = [0,halfRows)
+        val noMans = dimension.row % 2 == 1
+        if (row < halfRows) return SOUTH
+        if (noMans && (row == halfRows)) return ZERO
+        if (row > halfRows+1) return NORTH // north = [halfRows, dimRow)
+        return ZERO
+    }
 
     /**
      * Rotate position anticlockwise by 90 degrees
@@ -72,9 +107,13 @@ data class Vector2D(val col: Int, val row: Int)
         return Vector2D(col, -row)
     }
 
-
-    fun getIndex(dimension: Vector2D): Int {
-        // require() {"INCORRECT USAGE: position must be within dimension"}
+    /**
+     * To index
+     *
+     * @param dimension
+     * @return
+     */
+    fun toIndex(dimension: Vector2D): Int {
         return col + row * dimension.col
     }
 
@@ -98,6 +137,8 @@ data class Vector2D(val col: Int, val row: Int)
         val SOUTH_DIAGONALS = listOf(SE, SW)
         val EAST_DIAGONALS = listOf(NE, SE)
         val WEST_DIAGONALS = listOf(NW, SW)
+
+        val ZERO = Vector2D(0,0)
 
         /**
          * Get direction diagonals (ie NORTH -> NE and NW)
@@ -140,6 +181,40 @@ data class Vector2D(val col: Int, val row: Int)
          */
         fun fromIndex(index: Int, dimension: Vector2D):Vector2D {
             return Vector2D(index % dimension.col, index / dimension.row)
+        }
+
+        /**
+         * Find middle ranges (the middle 2x2 or 3x3 area of top or bottom of board)
+         *
+         * @param dimension
+         * @param locationVector2D
+         * @return
+         */
+        fun findMiddleRanges(dimension: Vector2D, locationVector2D: Vector2D): List<Vector2D> {
+            val ranges:MutableList<Vector2D> = mutableListOf()
+            // taking the middle cols
+            val mid = dimension.col /2
+            val start = mid - 1
+            val midCols= if (dimension.col %2 == 0) arrayOf(start, start + 1) else arrayOf(start, start + 1, start + 2)
+            // taking rows (from edge of the board)
+            var rows:Array<Int> = arrayOf()
+            if (locationVector2D == NORTH) {
+                // use the top X rows
+                val endIdx = dimension.row -1
+                rows = if (dimension.row %2 == 0) arrayOf(endIdx,endIdx-1) else arrayOf(endIdx,endIdx-1,endIdx-2)
+            }
+            if (locationVector2D == SOUTH) {
+                // use bottom X rows
+                rows = if (dimension.row %2 == 0) arrayOf(0,1) else arrayOf(0,1,2)
+            }
+            // now combine rows and cols into vectors (that may be out of bounds)
+            for (r in rows) {
+                for (c in midCols) {
+                    ranges.add(Vector2D(c, r))
+                }
+            }
+
+            return ranges
         }
     }
 }
